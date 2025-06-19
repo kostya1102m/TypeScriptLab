2de2324051419aee87eaeecd66aa616bdaf11afc
mercury <122353133+kostya1102m@users.noreply.github.com>
2025-06-20 02:43:20 +1000

    final

--- practicum_mui/src/App.tsx
+++ practicum_mui/src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './images/logo.svg';
+import Navbar from './components/Navbar';
+import './styles/App.css';
+import Gallery from './components/Gallery';
+import Content from './components/Content';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar active="1"/>
+      <Gallery/>
+      <Content/>
+      <Footer/>
     </div>
   );
 }
